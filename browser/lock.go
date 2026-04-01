21cdf0beeecc1204c62355446dbda9f339dab3b8
Vaayne <liu.vaayne@gmail.com>
2026-04-01 23:26:07 +0800

    ✨ feat: add browser state store and locks

---
+++ browser/lock.go
@@ -0,0 +1,42 @@
+package browser
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+type fileLock struct {
+	file *os.File
+}
+
+func lockFile(path string) (*fileLock, error) {
+	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+		return nil, fmt.Errorf("create lock dir: %w", err)
+	}
+
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0o600)
+	if err != nil {
+		return nil, fmt.Errorf("open lock file: %w", err)
+	}
+
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil {
+		_ = file.Close()
+		return nil, fmt.Errorf("lock file: %w", err)
+	}
+
+	return &fileLock{file: file}, nil
+}
+
+func (l *fileLock) Unlock() error {
+	if l == nil || l.file == nil {
+		return nil
+	}
+	err := syscall.Flock(int(l.file.Fd()), syscall.LOCK_UN)
+	closeErr := l.file.Close()
+	if err != nil {
+		return err
+	}
+	return closeErr
+}
